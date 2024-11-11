2d11f31de020cbae90106646f28a1f4e919c2498
untoldextacy <vanlarec1991@gmail.com>
2024-11-11 03:12:16 -0500

    fixing ports

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,11 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ['react-router-dom'],
-    },
-  },
-  base: './',
   server: {
-    port: process.env.PORT || 3000,
-    host: '0.0.0.0'
-  },
-});
-
+    port: 3000,
+    open: true
+  }
+})
