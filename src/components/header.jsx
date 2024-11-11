b3d53fc917a2ade2a76781b9896719fa85302f9e
untoldextacy <vanlarec1991@gmail.com>
2024-11-11 08:07:20 -0500

    fixed file names

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import Navigation from './Navigation';
+import Navigation from './navigation';
 import '../styles/Header.css';
 
 const Header = () => {
